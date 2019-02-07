d2a2f886e3a54bb8619c966ae4e396de46499059
anwaaralikhan <manwaaronline@gmail.com>
2019-02-06 21:23:00 -0500

    Basic Google Protocol Buffers Project 1

---
+++ grpc-basic/src/main/java/com/dc/grpc/basic/proto/BasicMain.java
@@ -0,0 +1,7 @@
+package com.dc.grpc.basic.proto;
+
+public class BasicMain {
+	public static void main(String[] args) {
+		System.out.println("Hello World");	
+	}
+}
---
+++ grpc-basic/src/test/java/com/dc/grpc/basic/GrpcBasicApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dc.grpc.basic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GrpcBasicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
